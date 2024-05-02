06edb9f39d4eb2e78afda29d27d0f56707176bed
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2024-05-02 16:58:37 +0700

    fix(db, entities): foreignkey problem

--- sugar_stream/internal/entities/sweet_favors_entities.go
+++ sugar_stream/internal/entities/sweet_favors_entities.go
@@ -1,32 +1,35 @@
 package entities
 
 type User struct {
-	UserID 			*uint 		`gorm:"primaryKey;not null;autoIncrement"`
-	Username   		*string
-	Password 		*string
-	Email 			*string
-	Firstname 		*string
-	Lastname 		*string
-	PhoneNum  		*string
-	UserPic 		*string
-	PromptPayAcc 	*string
-	PromptPayQR		*string
+	UserID       *uint `gorm:"primaryKey;autoIncrement"`
+	Username     *string
+	Password     *string
+	Email        *string
+	Firstname    *string
+	Lastname     *string
+	PhoneNum     *string
+	UserPic      *string
+	PromptPayAcc *string
+	PromptPayQR  *string
+	Wishlist     []Wishlist `gorm:"foreignKey:UserID"`
+	Followers    []Follow   `gorm:"foreignKey:UserID"`
+	Following    []Follow   `gorm:"foreignKey:FollowingID"`
 }
 
 type Wishlist struct {
-	WishlistID		*uint		`gorm:"primaryKey;not null;autoIncrement"`
-	UserID			*uint		
-	user 			User 		`gorm:"foreignKey:UserID"`
-	Itemname		*string
-	Quantity		*uint
-	Price			*uint
-	LinkURL			*string
-	ItemPic			*string
-	AlreadyBought	*bool
+	WishlistID    *uint `gorm:"primaryKey;autoIncrement"`
+	UserID        *uint `gorm:"not null"`
+	Itemname      *string
+	Quantity      *uint
+	Price         *uint
+	LinkURL       *string
+	ItemPic       *string
+	AlreadyBought *bool
 }
 
 type Follow struct {
-	UserID			*uint
-	user 			User 		`gorm:"foreignKey:UserID"`
-	FollowingID		*uint
+	UserID      *uint `gorm:"not null"`
+	User        User  `gorm:"foreignKey:UserID"`
+	FollowingID *uint `gorm:"not null"`
+	Following   User  `gorm:"foreignKey:FollowingID"`
 }
