256f3beeef3fcfd22ee25db5dd95709af9a1650c
Snailoba <fameeverywhere@gmail.com>
2024-05-03 22:19:56 +0700

    Update wish grant page
    Almost done
    Needs swiper thing

--- sweet_favors/lib/widgets/wish_grant_widget.dart
+++ sweet_favors/lib/widgets/wish_grant_widget.dart
@@ -15,46 +15,61 @@ class WishGrant extends StatelessWidget {
           margin: const EdgeInsets.only(bottom: 25),
           child: Column(
             children: [
-              Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  child: Column(children: [
-                    Image.asset(
-                      'assets/myGirl.png',
-                      width: 200,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                    Text(
-                      price,
-                      style: const TextStyle(
-                        fontSize: 14.0,
-                        color: colorUse.textColorSecondary,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 15.0,
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            offset: Offset(8, 5),
-                          )
-                        ],
+              Container(
+                height: MediaQuery.of(context).size.height / 1.5,
+                // decoration: BoxDecoration(
+                //   border: Border.all(
+                //       color: const Color(0xFF000000),
+                //       width: 4.0,
+                //       style: BorderStyle.solid), //Border.all
+                //   /*** The BorderRadius widget  is here ***/
+                //   borderRadius: BorderRadius.all(
+                //     Radius.circular(10),
+                //   ),
+                //   color: Color.fromARGB(255, 255, 115, 232),
+                // ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.asset(
+                            'assets/myGirl.png',
+                            width: 200,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          )),
+                      Text(
+                        price,
+                        style: const TextStyle(
+                          fontSize: 14.0,
+                          color: colorUse.textColorSecondary,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 15.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              offset: Offset(8, 5),
+                            )
+                          ],
+                        ),
                       ),
-                    )
-                  ])),
-              Center(
-                  child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const WishDetails(),
-                      // builder: (context) => WishDetails(product: product, grantBy: grantBy),
-                    ),
-                  );
-                },
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('View wish detail'),
-                ),
-              ))
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const WishDetails(),
+                              // builder: (context) => WishDetails(product: product, grantBy: grantBy),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('View wish detail'),
+                        ),
+                      )
+                    ]),
+              )
             ],
           )),
     );
