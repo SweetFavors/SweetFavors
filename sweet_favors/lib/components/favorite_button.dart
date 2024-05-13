e6a3c690c12a9a693623009f007488773972ad50
nsennes <panita.chavik@gmail.com>
2024-05-13 22:29:00 +0700

    favorite button on discover page

---
+++ sweet_favors/lib/components/favorite_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sweet_favors/Utils/color_use.dart';
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool _isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {
+        setState(() {
+          _isFavorite = !_isFavorite;
+        });
+      },
+      icon: Icon(
+        _isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: _isFavorite
+            ? colorUse.appBarColor
+            : colorUse.appBarColor, // Change color if favorite
+      ),
+      label: SizedBox.shrink(),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: _isFavorite
+            ? Colors.grey[300]
+            : null, // Change background color if favorite
+        shape: const CircleBorder(),
+        padding: EdgeInsets.zero,
+        alignment: Alignment.center,
+      ),
+    );
+  }
+}
--- sweet_favors/lib/widgets/wish_grant_widget.dart
+++ sweet_favors/lib/widgets/wish_grant_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:sweet_favors/Utils/color_use.dart';
+import 'package:sweet_favors/components/favorite_button.dart';
 
 class WishGrant extends StatelessWidget {
   final String price;
@@ -17,51 +18,76 @@ class WishGrant extends StatelessWidget {
     return SizedBox(
       height: 800,
       child: Card(
-          color: const Color.fromARGB(208, 249, 235, 251),
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Flexible(
-                      flex: 4,
-                      fit: FlexFit.loose,
-                      child: Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.network(
-                                pic,
-                                width: 500,
-                                height: 200,
-                                fit: BoxFit.fill,
-                              ))),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Flexible(
-                  fit: FlexFit.loose,
-                  flex: 1,
-                  child: Column(
-                    children: [
-                      Text(
-                        price,
-                        style: const TextStyle(
-                            fontSize: 18.0,
-                            color: colorUse.textFriend,
-                            fontWeight: FontWeight.bold),
+        color: const Color.fromARGB(208, 249, 235, 251),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 4.8,
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.network(
+                          pic,
+                          width: 500,
+                          height: 200,
+                          fit: BoxFit.fill,
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Positioned(
+                      top: 10.0,
+                      right: 1.0,
+                      child: FavoriteButton(),
+                      // child: ElevatedButton.icon(
+                      //   onPressed: () {
+                      //     // Handle favorite button tap
+                      //   },
+                      //   label: const SizedBox.shrink(), // Empty label
+                      //   style: ElevatedButton.styleFrom(
+                      //     shape: const CircleBorder(),
+                      //   ),
+                      //   icon: Row(
+                      //     mainAxisAlignment: MainAxisAlignment.center,
+                      //     mainAxisSize: MainAxisSize.min,
+                      //     children: [
+                      //       const Icon(
+                      //         Icons.favorite_border,
+                      //         color: colorUse.appBarColor,
+                      //       ),
+                      //     ],
+                      //   ),
+                      // ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          )),
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Flexible(
+                fit: FlexFit.loose,
+                flex: 1,
+                child: Column(
+                  children: [
+                    Text(
+                      price,
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        color: colorUse.textFriend,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
