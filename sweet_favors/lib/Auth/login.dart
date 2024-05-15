e7733c02a14c56cb5337bf53ea0f8ae938cedbda
nsennes <panita.chavik@gmail.com>
2024-05-16 01:36:52 +0700

    welcome page

--- sweet_favors/lib/Auth/login.dart
+++ sweet_favors/lib/Auth/login.dart
@@ -76,90 +76,94 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 25),
-              //app name0
-              const Text(
-                "Sign In",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 90),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 25),
+                //app name0
+                const Text(
+                  "Sign In",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
 
-              const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-              //email textfield
-              MyTextField(
-                hintText: 'Enter Email',
-                obscureText: false,
-                controller: emailController,
-              ),
+                //email textfield
+                MyTextField(
+                  hintText: 'Enter Email',
+                  obscureText: false,
+                  controller: emailController,
+                ),
 
-              const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-              //password textfield
-              MyTextField(
-                hintText: 'Enter Password',
-                obscureText: true,
-                controller: passwordController,
-                iconData: Icons.remove_red_eye_outlined,
-                onIconPressed: () {
-                  // Callback function to be called when the icon is pressed
-                },
-              ),
+                //password textfield
+                MyTextField(
+                  hintText: 'Enter Password',
+                  obscureText: true,
+                  controller: passwordController,
+                  iconData: Icons.remove_red_eye_outlined,
+                  onIconPressed: () {
+                    // Callback function to be called when the icon is pressed
+                  },
+                ),
 
-              const SizedBox(height: 25),
+                const SizedBox(height: 25),
 
-              //forgot password
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.end,
-              //   children: [
-              //     Text(
-              //       "Forgot Password",
-              //       style: TextStyle(
-              //         color: Theme.of(context).colorScheme.secondary,
-              //       ),
-              //     )
-              //   ],
-              // ),
+                //forgot password
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.end,
+                //   children: [
+                //     Text(
+                //       "Forgot Password",
+                //       style: TextStyle(
+                //         color: Theme.of(context).colorScheme.secondary,
+                //       ),
+                //     )
+                //   ],
+                // ),
 
-              const SizedBox(height: 25),
+                const SizedBox(height: 25),
 
-              //sign in button
-              MyButton(
-                text: "Sign In",
-                width: 400,
-                height: 58,
-                onTap: _login,
-              ),
+                //sign in button
+                MyButton(
+                  text: "Sign In",
+                  width: 400,
+                  height: 58,
+                  onTap: _login,
+                ),
 
-              const SizedBox(height: 40),
+                const SizedBox(height: 40),
 
-              //register here
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Don't have an account? ",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: const Text(
-                      "Register Here",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF4461F2),
+                //register here
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account? ",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        "Register Here",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF4461F2),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
